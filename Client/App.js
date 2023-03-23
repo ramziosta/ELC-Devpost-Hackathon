9fcc7b943dae8b8adb56cc0f44ff15b02bd37268
ramzi <ramziosta@gmail.com>
2023-03-23 15:38:46 -0400

    edits, .env not closed till judging

--- Client/App.js
+++ Client/App.js
@@ -1,16 +1,85 @@
 import React, { createContext, useEffect, useState } from "react";
+import { useFonts } from "expo-font";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import OnBoardingNavigation from "./routes/OnBoardingNavigation";
+import * as SplashScreen from 'expo-splash-screen';
+
 
 const Stack = createNativeStackNavigator();
 
+export const AtlasContext = createContext(undefined);
+
 const App = () => {
-  const atlasToken = getAtlasToken();
+  const [fontsLoaded] = useFonts({
+    Poppins: require("./assets/fonts/Poppins-Regular.ttf"),
+  });
+
+  const [appIsReady, setAppIsReady] = useState(false);
+  const [atlasToken, setAtlasToken] = useState("");
+  const [refreshToken, setRefreshToken] = useState("");
+
+  const getToken = async () => {
+    try {
+      const response = await fetch(
+        "https://us-central1.gcp.realm.mongodb.com/api/client/v2.0/app/elc-makeup-app-kerwc/auth/providers/anon-user/login"
+      );
+      const json = await response.json();
+      console.log(json);
+      setAtlasToken(json.access_token);
+      setRefreshToken(json.refresh_token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const reauthToken = async () => {
+    try {
+      const response = await fetch(
+        "https://us-central1.gcp.realm.mongodb.com/api/client/v2.0/auth/session",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${refreshToken}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      const json = await response.json();
+      setAtlasToken(json.access_token);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    async function prepareApp() {
+      try {
+        if (!fontsLoaded) {
+          throw new Error("Fonts not loaded");
+        }
+        await getToken();
+      } catch (error) {
+        console.error(error);
+      } finally {
+        setAppIsReady(true);
+        SplashScreen.hideAsync();
+      }
+    }
+
+    SplashScreen.preventAutoHideAsync()
+      .then(() => prepareApp())
+      .catch((error) => console.error(error));
+  }, [fontsLoaded]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
     <NavigationContainer>
       <AtlasContext.Provider value={atlasToken}>
-       <OnBoardingNavigation />
+        <OnBoardingNavigation />
       </AtlasContext.Provider>
     </NavigationContainer>
   );
@@ -18,41 +87,47 @@ const App = () => {
 
 export default App;
 
-
-// const theme = {
-//   ...DefaultTheme,
-//   colors: {
-//     ...DefaultTheme.colors,
-//     background: "grey",
-//   },
-// };
-
-export const AtlasContext = createContext(undefined);
-
 export const getAtlasToken = () => {
   const [atlasToken, setAtlasToken] = useState("");
-  const [refreshToken, setRefreshToken] = useState("")
+  const [refreshToken, setRefreshToken] = useState("");
   const getToken = async () => {
     try {
-      const response = await fetch("https://us-central1.gcp.realm.mongodb.com/api/client/v2.0/app/elc-makeup-app-kerwc/auth/providers/anon-user/login");
+      const response = await fetch(
+        "https://us-central1.gcp.realm.mongodb.com/api/client/v2.0/app/elc-makeup-app-kerwc/auth/providers/anon-user/login"
+      );
       const json = await response.json();
+
       setAtlasToken(json.access_token);
       setRefreshToken(json.refresh_token);
     } catch (error) {
       console.error(error);
     }
   };
+
   const reauthToken = async () => {
     try {
-      const response = await fetch("https://us-central1.gcp.realm.mongodb.com/api/client/v2.0/auth/session", {method: "POST", token: refreshToken });
+      const response = await fetch(
+        "https://us-central1.gcp.realm.mongodb.com/api/client/v2.0/auth/session",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${refreshToken}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
       const json = await response.json();
       setAtlasToken(json.access_token);
     } catch (error) {
       console.error(error);
     }
   };
+  
   useEffect(() => {
     getToken();
   }, []);
+  
   return atlasToken;
-}
+  };
+
+  
